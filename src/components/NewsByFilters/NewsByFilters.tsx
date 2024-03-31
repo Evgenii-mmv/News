8e7a8eaa1f01bb3431f341901b8be5e1f52f85de
Totter <tottercotter@gmail.com>
2024-04-01 00:55:55 +0300

    React+TS

---
+++ src/components/NewsByFilters/NewsByFilters.tsx
@@ -0,0 +1,60 @@
+import { getNews } from "../../api/apiNews";
+import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants";
+import { useDebounce } from "../../helpers/hooks/useDebounce";
+import { useFetch } from "../../helpers/hooks/useFetch";
+import { useFilters } from "../../helpers/hooks/useFilters";
+import { NewsApiResponse, ParamsType } from "../../interfaces";
+import NewsFilters from "../NewsFilters/NewsFilters";
+import NewsList from "../NewsList/NewsList";
+import PaginationWrapper from "../PaginationWrapper/PaginationWrapper";
+import styles from "./styles.module.css";
+
+const NewsByFilters = () => {
+  const { filters, changeFilter } = useFilters({
+    page_number: 1,
+    page_size: PAGE_SIZE,
+    category: null,
+    keywords: "",
+  });
+
+  const debouncedKeyword = useDebounce(filters.keywords, 2000);
+
+  const { data, isLoading } = useFetch<NewsApiResponse, ParamsType>(getNews, {
+    ...filters,
+    keywords: debouncedKeyword,
+  });
+  const handleNextPage = () => {
+    if (filters.page_number < TOTAL_PAGES) {
+      changeFilter("page_number", filters.page_number + 1);
+    }
+  };
+
+  const handlePastPage = () => {
+    if (filters.page_number > 1) {
+      changeFilter("page_number", filters.page_number - 1);
+    }
+  };
+
+  const handlePageClick = (pageNumber: number) => {
+    changeFilter("page_number", pageNumber);
+  };
+
+  return (
+    <section className={styles.section}>
+      <NewsFilters filters={filters} changeFilter={changeFilter} />
+      <PaginationWrapper
+        top
+        bottom
+        currentPage={filters.page_number}
+        handleNextPage={handleNextPage}
+        handlePastPage={handlePastPage}
+        handlePageClick={handlePageClick}
+        totalPages={TOTAL_PAGES}
+      >
+        <NewsList isLoading={isLoading} news={data?.news} />
+      </PaginationWrapper>
+    </section>
+  );
+};
+
+export default NewsByFilters;
